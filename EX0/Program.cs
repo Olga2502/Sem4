48b3cdcaa8f7873691b21490ac139bdc89e24ff5
Olga <101519229+Olga2502@users.noreply.github.com>
2023-02-19 22:39:00 +0300

    Решение задачи на подсчет количесва цифр в числе

---
+++ EX0/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число : ");
+int a = int.Parse(Console.ReadLine()!);
+int cont = 1;
+
+while(a >= 10 || a <= -10 )
+{
+    a = a / 10;
+    cont ++;
+}
+Console.WriteLine($"Количество цифр {cont}");
