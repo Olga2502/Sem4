238076cc7d30d15d661367a0b995579a9d636bad
Olga <101519229+Olga2502@users.noreply.github.com>
2023-02-19 23:09:53 +0300

    Решение зажачи на произведение цифр в числе

---
+++ EX4/Program.cs
@@ -0,0 +1,25 @@
+﻿//Написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+Console.WriteLine("Введите число : ");
+int a = int.Parse(Console.ReadLine()!);
+int b = 1;
+
+if(a > 1)
+{
+    for(int i = 1; i <= a; i++)
+    {
+    b = b * i;
+    //Console.WriteLine($"Произведение чисел от 1 до {i} равно  {b}");
+    }
+    Console.WriteLine($"Произведение чисел от 1 до {a} равно  {b}");
+}
+else
+{
+   //for(int i = 1; i >= a; i--)
+   //{
+    //b = b * i;
+    //Console.WriteLine($"Произведение чисел от 1 до {i} равно  {b}");
+    //}
+    Console.WriteLine($"Произведение чисел от 1 до {a} равно  0"); 
+}
