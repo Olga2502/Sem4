d5ae78a54d755798c63f47dd12e2e78347acde07
Olga <101519229+Olga2502@users.noreply.github.com>
2023-02-19 20:40:38 +0300

    Решение задачи с возведением в степень

---
+++ EX3/Program.cs
@@ -0,0 +1,28 @@
+﻿//Возведите число А в натуральную степень B используя цикл
+
+Console.Clear();
+Console.WriteLine("Введите число : ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите степень: ");
+int b = int.Parse(Console.ReadLine()!);
+
+int c = 1;
+
+if(b >= 0)
+{
+    for(int i = 1; i <= b; i++)
+    {
+        c *= a;
+    }
+    Console.WriteLine($"{c}");
+}
+else
+{
+    
+    for(int i = 0; i >= b; i--)
+    {
+        c *= a;
+    }
+    Console.WriteLine($"1/{c}");
+}
