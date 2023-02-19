5c7e5856f3484d906e9452f76b8ea4665400ab11
Olga <101519229+Olga2502@users.noreply.github.com>
2023-02-19 19:50:53 +0300

    Задача на суммирования с циклом

---
+++ EX1/Program.cs
@@ -0,0 +1,27 @@
+﻿// // Напишите программу, которая принимает на вход
+// число (N) и выдаёт таблицу квадратов чисел
+// от 1 до N.
+
+Console.Clear();
+Console.WriteLine("Введите число : ");
+int a = int.Parse(Console.ReadLine()!);
+int sm =  0;
+
+if(a > 0)
+{
+    for (int i = 0; i <= a; i++)
+    {
+        sm = sm + i;    
+    }
+
+}
+
+else
+{
+    for (int i = 0; i >= a; i--)
+    {
+        sm = sm + i;
+    }
+}
+
+Console.WriteLine($"Сумма чисел   {sm} ");
